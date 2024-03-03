1d24847370308a2f3ec6c62c5268646757ab5324
Carlos2442001 <cc.carlosangel@gmail.com>
2024-03-03 00:17:49 -0400

    avance de web 3

---
+++ rest_server.py
@@ -0,0 +1,40 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
+
+estudiantes = [
+    {
+        "id": 1,
+        "nombre": "Pedrito",
+        "apellido": "García",
+        "carrera": "Ingeniería de Sistemas",
+    },
+]
+
+class RESTRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/lista_estudiantes':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps(estudiantes).encode('utf-8'))
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps({"Error XXX": "Ruta no existente"}).encode('utf-8'))
+            
+    
+            
+def run_server(port = 8000):
+    try:
+        server_address = ('', port)
+        httpd = HTTPServer(server_address, RESTRequestHandler)
+        print(f'Iniciando servidor web en http://localhost:{port}/')
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print('Apagando servidor web')
+        httpd.socket.close()
+
+
+if __name__ == "__main__":
+    run_server()
---
+++ soap_client.py
@@ -0,0 +1,12 @@
+from zeep import Client 
+
+client =Client(
+    "http://localhost:8000/"
+)
+
+result = client.service.Saludar("Angelito")
+result2 = client.service.Sumar(1,2)
+result3 = client.service.EsPalindromo('A luna ese anula')
+print(result)
+print(result2)
+print(result3)
